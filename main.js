3dc06861e86550ce05723632f0dc7121f02fe3ee
railsonferoldi <r.feroldi@escola.pr.gov.br>
2026-06-30 16:43:40 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+
+const numeroSenha = document.querySelectorAll('.parametro-senha__texto');
+
+botoes[0].onclick = diminuiTamanho;
+
+function diminuiTamanho(){ 
+
+}
+console.log(botoes);
